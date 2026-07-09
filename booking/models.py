24e1bebfab940a8757c987d60f31277fe5ca5671
Ngor GechMuy <ngorgechmuy@gmail.com>
2026-07-09 12:15:45 +0700

    Update models.py

--- booking/models.py
+++ booking/models.py
@@ -25,11 +25,3 @@ class Booking(models.Model):
     def __str__(self):
         return f"{self.email} - {self.vehicle} ({self.status})"
 
-class Promo(models.Model):
-    code = models.CharField(max_length=50, unique=True)
-    discount_percent = models.IntegerField()
-    is_used = models.BooleanField(default=False)
-    created_at = models.DateTimeField(auto_now_add=True)
-
-    def __str__(self):
-        return f"{self.code} ({self.discount_percent}%) - Used: {self.is_used}"
